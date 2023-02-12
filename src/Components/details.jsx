96af245f1b1187a5b4fdd813c1dc8bfdac7628c6
Karlis <lacitiskarlis@gmail.com>
2023-02-13 00:31:39 +0200

    fixed details showing an array all in one string by using .join

--- src/Components/details.jsx
+++ src/Components/details.jsx
@@ -4,11 +4,14 @@ import * as S from "./style";
 export default function Details() {
   const location = useLocation();
   const recipe = location.state;
+
   return (
     <S.DetailsContainer>
       <S.DetailsHeading>{recipe.name}</S.DetailsHeading>
       <S.DetailsTime>Takes {recipe.time} minutes to cook</S.DetailsTime>
-      <S.DetailsIngredients>{recipe.ingredients}</S.DetailsIngredients>
+      <S.DetailsIngredients>
+        {recipe.ingredients.join(",")}
+      </S.DetailsIngredients>
       <S.DetailsMethod>{recipe.method}</S.DetailsMethod>
     </S.DetailsContainer>
   );
